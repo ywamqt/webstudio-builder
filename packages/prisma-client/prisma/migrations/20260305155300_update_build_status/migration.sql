c6fed858243cb66f8c74b96edaad67101cc8d18e
Matthew Johnson <matthewjj8@gmail.com>
2026-03-06 12:54:21 +1300

    fix update_build_status migration syntax error

--- packages/prisma-client/prisma/migrations/20260305155300_update_build_status/migration.sql
+++ packages/prisma-client/prisma/migrations/20260305155300_update_build_status/migration.sql
@@ -6,7 +6,7 @@ CREATE OR REPLACE FUNCTION update_build_status(
 BEGIN
   UPDATE "Build"
   SET "publishStatus" = publish_status
-  WHERE "id" = build_id
+  WHERE "id" = build_id;
 RETURN;
 END;
 $$ LANGUAGE plpgsql;
