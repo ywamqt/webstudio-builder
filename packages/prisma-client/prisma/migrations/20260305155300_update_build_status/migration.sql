21160c3adb277eae8ae6782a761349f3d64c9349
Matthew Johnson <matthewjj8@gmail.com>
2026-03-05 15:54:37 +1300

    fix update_build_status migration

--- packages/prisma-client/prisma/migrations/20260305155300_update_build_status/migration.sql
+++ packages/prisma-client/prisma/migrations/20260305155300_update_build_status/migration.sql
@@ -3,10 +3,11 @@ DROP FUNCTION IF EXISTS update_build_status;
 CREATE FUNCTION update_build_status(
   build_id text,
   publish_status text
-)
+) RETURNS void AS $$
 BEGIN
   UPDATE "Build"
   SET "publishStatus" = publish_status
   WHERE "id" = build_id
+-- RETURN;
 END;
 $$ LANGUAGE plpgsql;
