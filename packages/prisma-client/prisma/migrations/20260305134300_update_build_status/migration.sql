a60218538994848defd148281f3a1a6a914aed42
Matthew Johnson <matthewjj8@gmail.com>
2026-03-05 14:06:50 +1300

    add update_build_status DB function

---
+++ packages/prisma-client/prisma/migrations/20260305134300_update_build_status/migration.sql
@@ -0,0 +1,12 @@
+/* --- Migration Added by m8jj --- */
+DROP FUNCTION IF EXISTS update_build_status;
+CREATE FUNCTION update_build_status(
+  build_id text,
+  publish_status text
+)
+BEGIN
+  UPDATE "Build"
+  SET "publishStatus" = publish_status
+  WHERE "id" = build_id
+END;
+$$ LANGUAGE plpgsql;
