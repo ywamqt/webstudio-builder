03b0a5f291401e23404fb3bd0b2918b224d13d5d
Matthew Johnson <git@github.com>
2026-07-30 14:50:57 +1200

    add publishStatus update to project publishing

--- packages/domain/src/trpc/domain.ts
+++ packages/domain/src/trpc/domain.ts
@@ -82,7 +82,15 @@ export const domainRouter = router({
         if (input.destination === "saas") {
           const project = await projectApi.loadById(input.projectId, ctx);
           const domains = getVerifiedPublishDomains(project, input.domains);
-          await publishProject({ project, domains }, ctx);
+          const { build, proj, deploymentNotImplemented } = await publishProject({ project, domains }, ctx);
+          
+          /* --- Added by m8jj --- */
+          await ctx.postgrest.client
+              .from("Build")
+              .update({ publishStatus: "PUBLISHED" })
+              .eq("id", build.id);
+          /* ------ */
+          
           return { success: true as const };
         }
 
